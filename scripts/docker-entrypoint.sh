083588afb41779a5654bd7714e8ca93443d87ba0
n1a1b1 <n1a1b1@gmail.com>
2023-03-20 14:39:32 +0300

    stop try5

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# source /opt/backarosa/backup
 cat << "EOF"
 ██╗     ██████╗  ██████╗ ███████╗██╗  ██╗
 ██║     ██╔══██╗██╔════╝ ██╔════╝╚██╗██╔╝
@@ -15,8 +16,8 @@ env >> /etc/environment # This is needed for variables to be available to cron j
 # set a trap to catch SIGTERM and SIGINT signals so we can exit gracefully
 #exit the container when the script is finished
 trap 'exit' SIGTERM SIGINT
-if $signal == "done" ; then
-    kill -s SIGTERM 1
-fi
-exec "$@" # This is needed to run other scripts or commands when running the container like docker run -it --rm backarosa:latest /bin/bash, backup,restore,container_start,container_stop ..etc
+# if $signal == "done" ; then
+#     kill -s SIGTERM 1
+# fi
+exec "$@" & wait # This is needed to run other scripts or commands when running the container like docker run -it --rm backarosa:latest /bin/bash, backup,restore,container_start,container_stop ..etc
 #so when you run the container it will run only one command from scripts folder and exit, however commands in line 3 and 4 will run on every docker run - mandatory hard coded - 
